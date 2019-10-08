00458ad47d89d28bfbcf33bb80d8c6883dc0763b
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2019-10-08 07:23:12 +0100

    DW-2428

--- src/main/kotlin/app/batch/HBaseWriter.kt
+++ src/main/kotlin/app/batch/HBaseWriter.kt
@@ -5,10 +5,11 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.batch.item.ItemWriter
 import org.springframework.stereotype.Component
+import java.io.InputStream
 
 @Component
-class HBaseWriter(private val connection: Connection) : ItemWriter<EncryptedStream> {
-    override fun write(items: MutableList<out EncryptedStream>) {
+class HBaseWriter(private val connection: Connection) : ItemWriter<InputStream> {
+    override fun write(items: MutableList<out InputStream>) {
         logger.info("connection: '$connection'.")
         items.forEach {
             logger.info("it: '$it'.")
