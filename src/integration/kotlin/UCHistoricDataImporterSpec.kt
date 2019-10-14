1561545683bac46105c9393d5388eea4d96fb865
Daniel Chicot <dan.chicot@gmail.com>
2019-10-14 18:34:14 +0100

    Query the correct table.

--- src/integration/kotlin/UCHistoricDataImporterSpec.kt
+++ src/integration/kotlin/UCHistoricDataImporterSpec.kt
@@ -12,17 +12,17 @@ class Kafka2HBaseSpec: StringSpec(){
 
     init {
         "Number of records should match" {
-            val hbase = HbaseClient.connect()
-            val scan = Scan()
-           val rowCount =  hbase.connection.getTable(TableName.valueOf("k2hb:ingest-topic")).use { table ->
-                val  scanner = table.getScanner(scan)
+           val hbase = HbaseClient.connect()
+           val scan = Scan()
+           val rowCount =  hbase.connection.getTable(TableName.valueOf("k2hb:ingest")).use { table ->
+               val  scanner = table.getScanner(scan)
                val size = scanner.map { it }.size
                scanner.close()
                size
             }
 
             log.info("Row count : $rowCount")
-           rowCount shouldBe 4
+            rowCount shouldBe 4
         }
     }
 
