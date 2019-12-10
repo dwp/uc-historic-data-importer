58167c06b5c8e72cea41abf5ed71b91da583d1e4
Steven Burton <steven.burton1@dwp.gov.uk>
2019-12-10 14:55:41 +0000

    Add keep alive

--- src/test/kotlin/app/batch/HbaseWriterTest.kt
+++ src/test/kotlin/app/batch/HbaseWriterTest.kt
@@ -156,13 +156,7 @@ class HbaseWriterTest {
         val inputStream = ByteArrayInputStream(ByteArray(0))
         whenever(hBaseWriter.getBufferedReader(inputStream)).thenReturn(null)
 
-        val topic = "adb.collection".toByteArray()
-        val message = "message"
-        val formattedKey = "0000-0000-00001"
-        val key = formattedKey.toByteArray()
-        val message1 = message.toByteArray()
-
-        doNothing().`when`(hbase).putVersion(topic, key, message1, 100)
+        doNothing().whenever(hbase).putBatch(any())
         doNothing().whenever(manifestWriter).generateManifest(any(), any(), any(), any())
 
         val inputStreams = mutableListOf(DecompressedStream(inputStream, validFileName))
