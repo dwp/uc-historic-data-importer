abe7b271afeb66ed2d8870b70146834d35f1e17c
Daniel Chicot <dan.chicot@gmail.com>
2020-01-02 14:18:20 +0000

    Configurable KeySize

--- src/main/kotlin/app/batch/Config.kt
+++ src/main/kotlin/app/batch/Config.kt
@@ -17,6 +17,7 @@ object Config {
             set("zookeeper.znode.parent", getEnv("K2HB_HBASE_ZOOKEEPER_PARENT") ?: "/hbase")
             set("hbase.zookeeper.quorum", getEnv("K2HB_HBASE_ZOOKEEPER_QUORUM") ?: "zookeeper")
             setInt("hbase.zookeeper.port", getEnv("K2HB_HBASE_ZOOKEEPER_PORT")?.toIntOrNull() ?: 2181)
+            setInt("hbase.client.keyvalue.maxsize", getEnv("K2HB_HBASE_KEYVALUE_MAXSIZE")?.toIntOrNull() ?: 0)
         }
 
         val dataTable = getEnv("K2HB_HBASE_DATA_TABLE") ?: "k2hb:ingest"
