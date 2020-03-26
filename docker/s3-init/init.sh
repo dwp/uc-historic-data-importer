5c307ac8b65af91b3afceb8fe40c2cf434cab4c1
Daniel Chicot <dan.chicot@gmail.com>
2020-03-26 16:37:44 +0000

    backed out change.

--- docker/s3-init/init.sh
+++ docker/s3-init/init.sh
@@ -36,12 +36,8 @@ else
         aws_s3 cp a-spurious.file s3://${S3_BUCKET}/${S3_PREFIX}
         ls *.json.gz.enc | head -n1 | xargs rm -v
 
-        for file in *.json.gz.enc; do
-            aws_s3 cp $file s3://${S3_BUCKET}/${S3_PREFIX}files-data/
-        done
-
-        for file in *.json.encryption.json; do
-            aws_s3 cp $file s3://${S3_BUCKET}/${S3_PREFIX}files-metadata/
+        for file in *.json.gz.enc *.json.encryption.json; do
+            aws_s3 cp $file s3://${S3_BUCKET}/${S3_PREFIX}
         done
         aws_s3 ls $S3_BUCKET/$S3_PREFIX
     fi
