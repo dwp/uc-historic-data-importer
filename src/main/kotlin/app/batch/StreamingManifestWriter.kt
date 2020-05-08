7b89aafcc33e243408caf7fd5c62f01c1a7f79d3
Daniel Chicot <dan.chicot@gmail.com>
2020-05-08 11:44:06 +0100

    Log success of delete.

--- src/main/kotlin/app/batch/StreamingManifestWriter.kt
+++ src/main/kotlin/app/batch/StreamingManifestWriter.kt
@@ -26,7 +26,8 @@ open class StreamingManifestWriter {
                         s3.putObject(request)
                         logger.info("Written manifest", "attempt_number", "${attempts + 1}", "manifest_size", "$manifestSize", "s3_location", "s3://$manifestBucket/$manifestPrefix/$manifestFileName")
                         success = true
-                        manifestFile.delete()
+                        val deleted = manifestFile.delete()
+                        logger.info("Deleted manifest", "succeeded", "${deleted}")
                         return
                     }
                 }
