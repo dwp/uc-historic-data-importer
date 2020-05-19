d673c7e8106698794d25d0365a435b6190125993
Daniel Chicot <dan.chicot@gmail.com>
2020-05-19 20:01:08 +0100

    Retryable put (#112)
    * Use retryable put operation.
    
    * Use a retryable put object method.

--- src/test/kotlin/app/batch/StreamingManifestWriterTest.kt
+++ src/test/kotlin/app/batch/StreamingManifestWriterTest.kt
@@ -22,22 +22,15 @@ class StreamingManifestWriterTest {
         assertEquals(expected, actual)
     }
 
-    @Test
-    fun testManifestFileMetadataGeneration() {
-        val manifestWriter = StreamingManifestWriter()
-        val manifestFileName = "test-manifest-exporter/db.core.addressDeclaration.csv"
-        val actual = manifestWriter.manifestMetadata(manifestFileName, 1024)
-        assertEquals("text/plain", actual.contentType)
-        assertEquals(manifestFileName, actual.userMetadata["x-amz-meta-title"])
-        assertEquals(1024, actual.contentLength)
-    }
 
     @Test
     fun testManifestLogsException() {
         val root = LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger
         val mockAppender: Appender<ILoggingEvent> = mock()
         root.addAppender(mockAppender)
-        val s3 = mock<AmazonS3>()
+        val s3 = mock<AmazonS3> {
+            on { putObject(any(), any(), any<File>()) } doThrow RuntimeException("Failure")
+        }
         val manifestFile = mock<File> {
             on { name } doReturn "manifest.csv"
             on { toString() } doReturn "manifest.csv"
@@ -52,7 +45,7 @@ class StreamingManifestWriterTest {
         val formattedMessages = captor.allValues.map { it.formattedMessage }
 
         for (i in 1..10) {
-            assertTrue(formattedMessages.contains("Failed to write manifest\", \"attempt_number\":\"$i\", \"max_attempts\":\"10\", \"error_message\":\"null"))
+            assertTrue(formattedMessages.contains("Failed to write manifest\", \"attempt_number\":\"$i\", \"max_attempts\":\"10\", \"error_message\":\"Failure"))
         }
         assertTrue(formattedMessages.contains("Failed to write manifest after max attempts - giving up\", \"manifest_file\":\"manifest.csv\", \"max_attempts\":\"10"))
     }
